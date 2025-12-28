256147337b3fbf7c198ac9aa9bb9223e93d5c875
Mibah Cat“ <milaogou@gmail.com>
2025-12-28 18:46:20 +0800

    暂存最新实验结果

---
+++ benchmark_code/reproduce_imputation/block00_log/Electricity_log/HELIX_Electricity_.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -x paraai-n32-h-01-agent-[1,4,8,16,17,25,27,28,29,30,31]
+#SBATCH --job-name=HELIX_Electricity_block00_
+#SBATCH -o reproduce_imputation/block00_log/Electricity_log/HELIX_Electricity_.out
+#SBATCH -e reproduce_imputation/block00_log/Electricity_log/HELIX_Electricity_.log
+module purge
+module load miniforge3/24.1 
+module load compilers/cuda/12.1   compilers/gcc/11.3.0   cudnn/8.8.1.3_cuda12.x
+source activate py310pots
+export PYTHONUNBUFFERED=1
+export http_proxy=http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128
+export https_proxy=http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128   
+python -u train_model.py --model HELIX --dataset Electricity --dataset_fold_path data/generated_datasets/electricity_load_diagrams_rate00_step96_block_blocklen8 --saving_path reproduce_imputation/block00_log/Electricity_log --device cuda:0 
